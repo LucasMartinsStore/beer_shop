dac31b93b6aa04a641e968bf0722a124db731922
Lucas Martins <lucasifbaeuna@gmail.com>
2024-10-12 12:12:16 -0300

    fix: adjust input float component

--- src/app/core/shared/components/input-float/input-float.component.ts
+++ src/app/core/shared/components/input-float/input-float.component.ts
@@ -1,10 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-input-float',
   templateUrl: './input-float.component.html',
   styleUrl: './input-float.component.scss',
 })
-export class InputFloatComponent {
+export class InputFloatComponent implements OnInit {
   @Input() label = '';
+  @Input() inputId = '';
+
+  inputType = '';
+
+  ngOnInit(): void {
+    this.changedType();
+  }
+
+  changedType() {
+    const typeMap: { [key: string]: string } = {
+      senha: 'password',
+      email: 'email',
+    };
+
+    this.inputType = typeMap[this.label.toLocaleLowerCase()] || 'text';
+  }
 }
